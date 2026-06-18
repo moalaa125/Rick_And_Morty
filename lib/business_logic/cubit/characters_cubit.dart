370cb5bc990b7eef51a1a7d24421b8e59927dedb
moalaa125 <muhamedawadkassem@gmail.com>
2026-06-18 23:33:27 +0300

    implment state , cubit

--- lib/business_logic/cubit/characters_cubit.dart
+++ lib/business_logic/cubit/characters_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:rickandmorty/data/models/characters.dart';
+import 'package:rickandmorty/data/repository/characters_repository.dart';
 
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  final CharactersRepository charactersRepository;
+
+  List<Characters> characters = [];
+
+  CharactersCubit(this.charactersRepository) : super(CharactersInitial());
+
+  List<Characters> getAllCharacters() {
+    charactersRepository.getAllCharacters().then((charactersList) {
+      characters = charactersList;
+      emit(CharachterLoaded(charactersList));
+    });
+    return characters;
+  }
 }
--- lib/business_logic/cubit/characters_state.dart
+++ lib/business_logic/cubit/characters_state.dart
@@ -4,3 +4,8 @@ part of 'characters_cubit.dart';
 sealed class CharactersState {}
 
 final class CharactersInitial extends CharactersState {}
+
+class CharachterLoaded extends CharactersState {
+  final List<Characters> characters;
+  CharachterLoaded(this.characters);
+}
